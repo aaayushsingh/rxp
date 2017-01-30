43d243ddec36e710385c7d22a61778991b765b37
Ayush Singh <ayushsinghiii.94@gmail.com>
2017-01-31 01:19:58 +0530

    initial testing

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var express=require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request=require('request');
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'),function(){
+console.log("Your server is now running...");
+});
+
+app.use(bodyParser.json());
+
+
 app.get('/',function(req,res){
 
     console.log(req.method+" request received at "+req.url);
