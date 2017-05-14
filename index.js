bd302d6257af2fe0cc001c943c3989aa3a193c6a
Ayush Singh <ayushsinghiii.94@gmail.com>
2017-05-14 18:34:02 +0530

    afas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express=require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request=require('request');
+var router = express.Router();
 
 
 app.use(express.static(__dirname+'/required/'));
@@ -14,6 +15,25 @@ console.log(app.get('port'));
 
 app.use(bodyParser.json());
 
+
+app.get('/gu',function(req,res){
+
+	request({
+	     url:"https://sc.productrx.com/public/schema/survey",
+	     method:"GET",
+		 qs:{query:"survey",data_type:"JSON"}
+		 },function(err,response,body){
+	          if(err){
+		          console.log(err);
+				  res.status(300).json(error.apiError);
+	            }
+	          else{
+		          //console.log());
+				  var body=JSON.parse(body);
+				  res.status(200).json(body);
+	            }
+			});
+});
 
 app.get('/',function(req,res){
 
